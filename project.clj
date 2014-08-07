9f6b15752703138f1e2e97b1ec3cba84c92f6fdb
Klaus Hebsgaard <klaus@hebsgaard.dk>
2014-08-07 16:12:20 +0200

    starting to parse and send json more correctly

--- project.clj
+++ project.clj
@@ -3,9 +3,9 @@
       :url "http://blog.interlinked.org"
       :dependencies [[org.clojure/clojure "1.6.0"]
                      [compojure "1.1.6"]
-                     [ring/ring-json "0.1.2"]
+                     [ring/ring-json "0.3.1"]
                      [c3p0/c3p0 "0.9.1.2"]
-                     [cheshire "4.0.3"]
+                     [cheshire "5.3.1"]
                      [com.novemberain/monger "2.0.0"]]
       :plugins [[lein-ring "0.8.10"]]
       :ring {:handler clojure-rest.handler/app}
--- src/clojure_rest/models/document.clj
+++ src/clojure_rest/models/document.clj
@@ -1,30 +1,35 @@
 (ns clojure-rest.models.document
       (:import com.mchange.v2.c3p0.ComboPooledDataSource)
       (:use ring.util.response)
+    (:require [cheshire.core :refer :all]
+            [cheshire.generate :refer [add-encoder encode-str remove-encoder]])
     (:require [monger.core :as mg]
             [monger.collection :as mc])
+            (:require monger.json)
       (:import org.bson.types.ObjectId)
       (:require
                 [compojure.route :as route]))
+
+(cheshire.generate/add-encoder org.bson.types.ObjectId cheshire.generate/encode-str)
 
     (defn get-all-documents []
       (response
         (let [conn (mg/connect)
          db (mg/get-db conn "monger-test")]
-        (mc/find-maps db "documents"))))
+         (generate-string
+        (mc/find-maps db "documents")))))
 
     (defn get-document [id]
     (let [conn (mg/connect)
       db   (mg/get-db conn "monger-test")
       coll "documents"]
-  ;; find one document by id, as Clojure map
-  (mc/find-map-by-id db coll (ObjectId. id))))
+   (mc/find-map-by-id db coll (ObjectId. id))))
 
     (defn create-new-document [doc]
       (let [conn (mg/connect)
       db   (mg/get-db conn "monger-test")
       oid  (ObjectId.)
-      document  (assoc doc "source" "post")]
+      document  (assoc {} "title" (doc "title") "text" (doc "text"))]
       (mc/insert db "documents" (merge document {:_id oid})))
       {:status 200}
       )
